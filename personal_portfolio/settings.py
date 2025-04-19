06ca9b5804660969bfece39ef2913ecf7b2406ae
Supriya Maji <supriyamaji@Supriyas-MacBook-Air.local>
2025-04-19 11:53:23 +0530

    Update

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -140,10 +140,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional (recommended for production):
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
