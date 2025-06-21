957d21cc99f309f71c6b30cde29e9f11302e04f3
Supriya Maji <157310804+supriyamaji76@users.noreply.github.com>
2025-06-21 14:49:13 +0530

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['personal-portfolio-q9xw.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['personal-portfolio-lm50.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
